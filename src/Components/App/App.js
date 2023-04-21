34bea1d3fef22edc69e820b847a3883638f36749
sarahcrack <sarah.l.crack@googlemail.com>
2023-04-21 22:17:26 +0100

    created App and Dictionary components and basic text for page

---
+++ src/Components/App/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Dictionary from "../Dictionary/Dictionary";
+import bookshop from "../Images/bookshop.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">Dictionary Corner 📚</header>
+        <div className="Dictionary-content">
+          <main>
+            {" "}
+            <h1>Welcome to Dictionary Corner</h1>
+            <Dictionary />
+            <p>Here you can find all the words you need!</p>
+            <img src={bookshop} className="Bookshop img-fluid" alt="bookshop" />
+          </main>
+        </div>
+        <footer className="App-footer">Coded by Sarah Crack</footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Dictionary/Dictionary.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Dictionary() {
+  return <div className="Dictionary">Hello!</div>;
+}
+
+export default Dictionary;
