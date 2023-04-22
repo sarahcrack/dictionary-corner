64a4e91e8d75f3539cc32fd079b90ecaaae75142
sarahcrack <sarah.l.crack@googlemail.com>
2023-04-22 21:31:00 +0100

    created a form to search for a word and setKeyword state

--- src/Components/Dictionary/Dictionary.js
+++ src/Components/Dictionary/Dictionary.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 function Dictionary() {
-  return <div className="Dictionary">Hello!</div>;
+  const [keyword, setKeyword] = useState("");
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autofocus={true} onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
 }
 
 export default Dictionary;
