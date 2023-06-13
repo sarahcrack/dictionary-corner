7c6d0057660808d5002683e1c0f8d4119538c6d1
sarahcrack <sarah.l.crack@googlemail.com>
2023-06-13 20:14:19 +0100

    Photos component pulls photos for the image searched - still need to aort out Lucky Dip button

--- src/Components/Photos/Photos.js
+++ src/Components/Photos/Photos.js
@@ -1,6 +1,30 @@
+import "./Photos.css";
+
 function Photos(props) {
-  console.log(props.photos);
-  return "Hello from Photos";
+  if (props.photos) {
+    console.log(props.photos);
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt={photo.alt}
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default Photos;
